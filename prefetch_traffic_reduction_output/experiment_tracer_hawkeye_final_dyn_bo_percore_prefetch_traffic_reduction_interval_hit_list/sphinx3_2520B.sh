e9ed8b162440eee2c9d50e2c35e993e150343771
Caroline Li <cli23@cs.utexas.edu>
2023-08-17 22:20:28 -0500

    prefetch traffic reduction raw data backup

---
+++ prefetch_traffic_reduction_output/experiment_tracer_hawkeye_final_dyn_bo_percore_prefetch_traffic_reduction_interval_hit_list/sphinx3_2520B.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/u/cli23/thesis_research/ChampSim/bin/perceptron-no-bo_percore-hawkeye_final_dyn-1core -warmup_instructions 0000000 -simulation_instructions 200000000 -hide_heartbeat -traces /scratch/cluster/akanksha/CRCRealTraces/sphinx3_2520B.trace.gz &> /u/cli23/thesis_research/ChampSim/output/experiment_tracer_hawkeye_final_dyn_bo_percore_prefetch_traffic_reduction_interval_hit_list/sphinx3_2520B.txt
---
+++ prefetch_traffic_reduction_output/experiment_tracer_hawkeye_final_dyn_sisb_prefetch_traffic_reduction_interval_hit_list/zeusmp_600B.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/u/cli23/thesis_research/ChampSim/bin/perceptron-no-sisb-hawkeye_final_dyn-1core -warmup_instructions 0000000 -simulation_instructions 200000000 -hide_heartbeat -traces /scratch/cluster/akanksha/CRCRealTraces/zeusmp_600B.trace.gz &> /u/cli23/thesis_research/ChampSim/output/experiment_tracer_hawkeye_final_dyn_sisb_prefetch_traffic_reduction_interval_hit_list/zeusmp_600B.txt
